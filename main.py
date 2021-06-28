dbdd49571c8bf48625c8d2a8507da0664d3955e1
Theodor Johansson <theodor.johansson@praetorian.com>
2021-06-27 22:43:04 -0500

    Working server with one endpoint

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import os
 
 from flask import Flask
+import json
 
 
 def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+  # create and configure the app
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_mapping(
+    SECRET_KEY='dev',
+  )
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+  # a simple page that says hello
+  @app.route('/api/v1/vulnerability')
+  def get_all_vulnerabilities():
+    with open("vulnerabilities.json", "r") as fd:
+      data = fd.read()
 
-    return app
+      all_vulnerabilities = json.loads(data)
+      return str(all_vulnerabilities)
+
+
+  return app

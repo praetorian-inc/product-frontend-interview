c5eecb645c1c9cb06bc30d7369db5ef995247b98
Theodor Johansson <theodor.johansson@praetorian.com>
2021-06-28 11:38:02 -0500

    Frontend can parse data

--- server/main.py
+++ server/main.py
@@ -16,21 +16,21 @@ def create_app(test_config=None):
     with open("vulnerabilities.json", "r") as fd:
       data = fd.read()
 
-      all_vulnerabilities = json.loads(data)["data"]
-      return str(all_vulnerabilities)
+      all_vulnerabilities = json.loads(data)
+      return json.dumps(all_vulnerabilities)
 
   @app.route('/api/v1/vulnerability/<vulnerabilityId>')
   def get_vulnerability(vulnerabilityId):
     with open("vulnerabilities.json", "r") as fd:
       data = fd.read()
 
-      all_vulnerabilities = json.loads(data)["data"]
+      all_vulnerabilities = json.loads(data)
       vulns_with_id = [v for v in all_vulnerabilities if v["id"] == vulnerabilityId]
 
       if (len(vulns_with_id) == 0):
         return "null"
       else:
-        return str(vulns_with_id[0])
+        return json.dumps(vulns_with_id[0])
 
       return str(all_vulnerabilities)
 
